0b16d1f74d7aded469c12adea0aa2b4e84032689
Aniket <aniketkumar190@gmail.com>
2021-04-23 12:10:12 +0530

    Initial Status

---
+++ src/components/SearchBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {View,Text,StyleSheet} from 'react-native'
+
+const SearchBar = () => {
+    return (
+        <View style={styles.background}>
+            <Text>Search Screen</Text>'
+         </View>
+
+
+    )
+}
+
+const styles = StyleSheet.create({
+    background:{
+        backgroundColor:"grey"
+    }
+});
+
+export default SearchBar
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View,Text,StyleSheet} from 'react-native';
+import SearchBar from '../components/SearchBar';
+
+
+const SearchScreen = () =>{
+    return (<View>
+        
+        <SearchBar/>
+        <Text>This is Search Screen</Text></View>)
+}
+
+const styles=StyleSheet.create({});
+export default SearchScreen;
